17bd267a50a5887bedc94b10dd2538a8d93a9835
derik willamis <clachgigante@gmail.com>
2023-05-14 20:39:10 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imageBotaoTrocaDeTema = document.querySelector(".image-botton")
+
+botaoAlterarTema.addEventListener("click",() => {
+
+    const modoEscuroEstaAtivo = body.classList.
+    contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro")
+
+    if(modoEscuroEstaAtivo) {
+
+        imageBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png")
+
+    } else {
+        imageBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png")
+    }
+})
